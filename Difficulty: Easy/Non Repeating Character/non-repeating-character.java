e6a34ef69d8754a5902af304cecebfaf7e7c8dee
Nihal <nihalpuli557@gmail.com>
2026-08-16 19:09:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Non Repeating Character/non-repeating-character.java
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -1,11 +1,12 @@
+import java.util.*;
 class Solution {
     public char nonRepeatingChar(String s) {
-        int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++)
-            freq[s.charAt(i) - 'a']++;
-        for (int i = 0; i < s.length(); i++) {
-            if (freq[s.charAt(i) - 'a'] == 1)
-                return s.charAt(i);
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        for (char c : s.toCharArray()) {
+            if (map.get(c) == 1)
+                return c;
         }
         return '$';
     }
