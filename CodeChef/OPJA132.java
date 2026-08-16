2c0d801783eb234e88bd30ca7bbd8af349179db8
Nihal <nihalpuli557@gmail.com>
2026-08-16 16:15:13 +0530

    Add CodeChef: OPJA132

---
+++ CodeChef/OPJA132.java
@@ -0,0 +1,22 @@
+class Codechef {
+    public static void main(String[] args) {
+        String text = "Codechef";
+        // Format the text using UpperCaseFormatter
+        TextFormatter formatter = new UpperCaseFormatter
+            ();
+        System.out.println(formatter.format(text));
+        // Format the text using ReverseFormatter
+ Subtask Info
+Correct Answer
+Submission ID:
+1343338621
+Score:
+100
+Memory:
+37.4M
+Sub-Task Task # Result
+(time)
+1 0 Correct
+(0.03)
+Subtask Score: 100% Result - Correct
+Total Score = 100%
