e7e1e9909e786551c2297292304c1aefca4f665f
Nihal <nihalpuli557@gmail.com>
2026-09-02 21:36:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count of camel case characters/count-of-camel-case-characters.java
@@ -0,0 +1,12 @@
+class Sol {
+     int countCamelCase(String s) {
+         int count = 0;
+         for (int i = 0; i < s.length(); i++) {
+             char c = s.charAt(i);
+             if (c >= 'A' && c <= 'Z') {
+                 count++;
+             }
+         }
+         return count;
+     }
+ }
