49f780210cdbcb9776626464655ce09b4703d25f
Nihal <nihalpuli557@gmail.com>
2026-09-23 22:06:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Pair Sum in 2 Arrays/count-pair-sum-in-2-arrays.java
@@ -0,0 +1,25 @@
+class Solution
+{
+    public int countPairs(int[] a, int[] b, int x)
+    {
+        int i = 0;
+        int j = b.length - 1;
+        int count = 0;
+        while (i < a.length && j >= 0)
+        {
+            int sum = a[i] + b[j];
+
+            if (sum == x)
+            {
+                count++;
+                i++;
+                j--;
+            }
+            else if (sum < x)
+                i++;
+            else
+                j--;
+        }
+        return count;
+    }
+}
