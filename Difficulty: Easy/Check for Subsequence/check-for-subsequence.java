89868203274cf8dfd82b01d6022ae2062ae64aaa
Nihal <nihalpuli557@gmail.com>
2026-09-23 22:10:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check for Subsequence/check-for-subsequence.java
@@ -0,0 +1,17 @@
+class Solution
+{
+    public boolean isSubSeq(String A, String B)
+    {
+        int i = 0;
+        int j = 0;
+
+        while (i < A.length() && j < B.length())
+        {
+            if (A.charAt(i) == B.charAt(j))
+                i++;
+            j++;
+        }
+
+        return i == A.length();
+    }
+}
